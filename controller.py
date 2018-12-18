2bec8f9fd1de70bfc66b60f627c193b8563b6f18
Albert <alberthsies@gmail.com>
2018-12-18 20:07:53 +0800

    Debug

--- controller.py
+++ controller.py
@@ -38,7 +38,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    secM = TextSendMessage(text=sec)
+    secM = TextSendMessage(text=str(sec))
     line_bot_api.reply_message(event.reply_token, message)
     line_bot_api.reply_message(event.reply_token, secM)
 
--- littleMaruDAO.py
+++ littleMaruDAO.py
@@ -8,6 +8,7 @@ conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 def getToken (userID = "1"):
     cur = conn.cursor()
     cur.execute("SELECT line_bot_api, webhook_handler FROM line_data WHERE user_id = \'" + userID + "\';")
-    result = cur.fetchall()
-    return result
+    for i in cur.fetchall():
+        return i
+    return cur.fetchall()
 
