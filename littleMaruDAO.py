7940aa1c57b214ceba173a593c7733f697a02aee
Albert <alberthsies@gmail.com>
2018-12-18 19:44:10 +0800

    Try typo
    forget the `’’` before int

--- littleMaruDAO.py
+++ littleMaruDAO.py
@@ -7,7 +7,7 @@ conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 def getToken (userID = "1"):
     cur = conn.cursor()
-    cur.execute("SELECT line_bot_api, webhook_handler FROM line_data WHERE user_id = " + userID + ";")
+    cur.execute("SELECT line_bot_api, webhook_handler FROM line_data WHERE user_id = \'" + userID + "\';")
     result = cur.fetchall()
     return result
 
