08b49062d5147078afb85b51fe79c5f8d32adc8a
Albert <alberthsies@gmail.com>
2018-12-18 19:25:22 +0800

    Testing

--- controller.py
+++ controller.py
@@ -13,11 +13,11 @@ import littleManDAO
 app = Flask(__name__)
 sec = littleManDAO.getToken()
 # Channel Access Token
-#line_bot_api = LineBotApi('7QOb0ctgmnDIMgQn8f6xx5Sw8o/ubO7/2xad8HbcREROFTlmCTDQI87Y12nrTvXfwni4NfLyU5g6ZkmPMe7sRMjbbtXyg2wCHMVNKjf/vH7IR6GJrFyu8uMBuROMWJXWIJsbQQ51MIPFVoE2xqPUZwdB04t89/1O/w1cDnyilFU=')
-line_bot_api = LineBotApi(sec[0])
+line_bot_api = LineBotApi('7QOb0ctgmnDIMgQn8f6xx5Sw8o/ubO7/2xad8HbcREROFTlmCTDQI87Y12nrTvXfwni4NfLyU5g6ZkmPMe7sRMjbbtXyg2wCHMVNKjf/vH7IR6GJrFyu8uMBuROMWJXWIJsbQQ51MIPFVoE2xqPUZwdB04t89/1O/w1cDnyilFU=')
+#line_bot_api = LineBotApi(sec[0])
 # Channel Secret
-#handler = WebhookHandler('624fcd094f053724f59922cc96747faa')
-handler = WebhookHandler(sec[1])
+handler = WebhookHandler('624fcd094f053724f59922cc96747faa')
+#handler = WebhookHandler(sec[1])
 
 # listem on the income message /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -38,7 +38,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
+    secM = TextSendMessage(text=sec[1])
     line_bot_api.reply_message(event.reply_token, message)
+    line_bot_api.reply_message(event.reply_token, secM)
 
 import os
 if __name__ == "__main__":
